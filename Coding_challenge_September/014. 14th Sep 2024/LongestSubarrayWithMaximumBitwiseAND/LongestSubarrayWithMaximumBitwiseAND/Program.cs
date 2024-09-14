eaba3b7360d3f32742e23f8076d7b0fa1279d101
Hoang Minh <hoangminh30042003@gmail.com>
2024-09-14 13:03:00 +0700

    14th Sep 2024

---
+++ Coding_challenge_September/014. 14th Sep 2024/LongestSubarrayWithMaximumBitwiseAND/LongestSubarrayWithMaximumBitwiseAND/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        TestCase1();
+        TestCase2();
+    }
+
+    public static void TestCase1()
+    {
+        int[] nums = { 1, 2, 3, 3, 2, 2 };
+        int result = LongestSubarray(nums);
+        Console.WriteLine(result);
+    }
+
+    public static void TestCase2()
+    {
+        int[] nums = { 1, 2, 3, 4 };
+        int result = LongestSubarray(nums);
+        Console.WriteLine(result);
+    }
+
+    public static int LongestSubarray(int[] nums)
+    {
+        int maximumElement = nums.Max(x => x);
+        int maxLength = 0;
+        int currentLength = 0;
+
+        foreach (int num in nums)
+        {
+            if (num == maximumElement)
+            {
+                currentLength++;
+            }
+            else
+            {
+                maxLength = Math.Max(maxLength, currentLength);
+                currentLength = 0;
+            }
+        }
+
+        maxLength = Math.Max(maxLength, currentLength);
+        return maxLength;
+    }
+}
