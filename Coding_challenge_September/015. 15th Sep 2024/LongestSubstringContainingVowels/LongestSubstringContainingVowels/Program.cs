4c683d02158dc7b8822cbb7dca6f504f77885fd4
Hoang Minh <hoangminh30042003@gmail.com>
2024-09-18 14:24:22 +0700

    17th Sep 2024

---
+++ Coding_challenge_September/015. 15th Sep 2024/LongestSubstringContainingVowels/LongestSubstringContainingVowels/Program.cs
@@ -0,0 +1,76 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        TestCase1();
+        TestCase2();
+        TestCase3();
+    }
+
+    public static void TestCase1()
+    {
+        string s = "eleetminicoworoep";
+        PrintResult(s);
+    }
+
+    public static void TestCase2()
+    {
+        string s = "leetcodeisgreat";
+        PrintResult(s);
+    }
+
+    public static void TestCase3()
+    {
+        string s = "bcbcbc";
+        PrintResult(s);
+    }
+
+    public static void PrintResult(string s)
+    {
+        Console.WriteLine(FindTheLongestSubstring(s));
+    }
+
+    public static int FindTheLongestSubstring(string s)
+    {
+        Dictionary<char, int> vowels = new Dictionary<char, int>
+        {
+            {'a', 0},
+            {'e', 1},
+            {'i', 2},
+            {'o', 3},
+            {'u', 4}
+        };
+
+        // Current bitmask state (start with all vowels appearing an even number of times)
+        int currentMask = 0;
+        // Dictionary to store the first occurrence of each mask
+        Dictionary<int, int> seen = new Dictionary<int, int> { { 0, -1 } };
+
+        int maxLen = 0;
+
+        // Traverse the string
+        for (int i = 0; i < s.Length; i++)
+        {
+            char ch = s[i];
+
+            // If the character is a vowel, flip the corresponding bit
+            if (vowels.ContainsKey(ch))
+            {
+                currentMask ^= (1 << vowels[ch]);
+            }
+
+            // If this mask has been seen before, calculate the length of the substring
+            if (seen.ContainsKey(currentMask))
+            {
+                maxLen = Math.Max(maxLen, i - seen[currentMask]);
+            }
+            else
+            {
+                // Otherwise, store the first occurrence of this mask
+                seen[currentMask] = i;
+            }
+        }
+
+        return maxLen;
+    }
+}
---
+++ Coding_challenge_September/017. 17th Sep 2024/UncommonWordsFromTwoSentences/UncommonWordsFromTwoSentences/Program.cs
@@ -0,0 +1,52 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        TestCase();
+    }
+
+    public static void TestCase()
+    {
+        string s1 = "this apple is sweet";
+        string s2 = "this apple is sour";
+        string[] result = UncommonFromSentences(s1, s2);
+        Console.WriteLine(String.Join(", ", result));
+    }
+
+    public static string[] UncommonFromSentences(string s1, string s2)
+    {
+        Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+        void AddWords(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+        }
+
+        AddWords(s1);
+        AddWords(s2);
+
+        // Find uncommon words that appear exactly once
+        List<string> result = new List<string>();
+        foreach (var word in wordCount)
+        {
+            if (word.Value == 1)
+            {
+                result.Add(word.Key);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
