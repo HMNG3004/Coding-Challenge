19a5b505698ae19e9c7f93766a834cf182de62ce
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-03 09:32:14 +0700

    3-12-2024

---
+++ LeetCode/Coding_challenge_12_2024/3-12-2024/3-12-2024/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        RunTestCase();
+    }
+
+    public static void RunTestCase()
+    {
+        Console.WriteLine("Running Test Cases...");
+        TestCase("LeetcodeHelpsMeLearn", new int[] { 8, 13, 15 }, "Leetcode Helps Me Learn", "Test case 1:");
+        TestCase("spacing", new int[] { 0, 1, 2, 3, 4, 5, 6 }, " s p a c i n g", "Test case 2:");
+        TestCase("icodeinpython", new int[] { 1, 5, 7, 9 }, "i code in py thon", "Test case 3:");
+    }
+
+    public static void TestCase(string sentence, int[] spaces, string expected, string description)
+    {
+        var result = AddSpaces(sentence, spaces);
+        Console.WriteLine($"{description}: {(result == expected ? "Passed" : "Failed")}");
+    }
+
+
+    public static string AddSpaces(string s, int[] spaces)
+    {
+        var result = new StringBuilder();
+        int spaceIndex = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (spaceIndex < spaces.Length && i == spaces[spaceIndex])
+            {
+                result.Append(' ');
+                spaceIndex++;
+            }
+            result.Append(s[i]);
+        }
+
+        return result.ToString();
+    }
+}
---
+++ LeetCode/Coding_challenge_12_2024/3-12-2024/README.md
@@ -0,0 +1,9 @@
+# **Adding Spaces to a String**
+
+You are given a **0-indexed** string `s` and a **0-indexed** integer array `spaces` that describes the indices in the original string where spaces will be added. Each space should be inserted `before` the character at the given index.
+
+- For example, given `s = "EnjoyYourCoffee"` and `spaces = [5, 9]`, we place spaces before `'Y'` and `'C'`, which are at indices `5` and `9` respectively. Thus, we obtain `"Enjoy Your Coffee"`.
+
+Return the _modified_ string **_after_** the spaces have been added.
+
+View full question <a href="https://leetcode.com/problems/adding-spaces-to-a-string?envType=daily-question&envId=2024-12-03">here</a>
