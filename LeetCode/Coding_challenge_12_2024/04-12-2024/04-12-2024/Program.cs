14b82878235ff559c05b9d62cd95d516caa477bf
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-04 17:15:22 +0700

    04-12-2024

---
+++ LeetCode/Coding_challenge_12_2024/04-12-2024/04-12-2024/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Running Test Cases...");
+        TestCase("abc", "ad", true, "Test case 1:");
+        TestCase("zc", "ad", true, "Test case 2:");
+        TestCase("ab", "d", false, "Test case 3:");
+    }
+
+    public static void TestCase(string str1, string str2, bool expected, string description)
+    {
+        var result = CanMakeSubsequence(str1, str2);
+        Console.WriteLine($"{description}: {(result == expected ? "Passed" : "Failed")}");
+    }
+
+    public static bool CanMakeSubsequence(string str1, string str2)
+    {
+        int n = str1.Length, m = str2.Length;
+
+        if (n < m)
+            return false;
+
+        int i = 0, j = 0;
+
+        while (i < n && j < m)
+        {
+            char currentChar = str1[i];
+            char targetChar = str2[j];
+
+            // Match either the current character or its cyclic increment
+            if (currentChar == targetChar || (char)((currentChar - 'a' + 1) % 26 + 'a') == targetChar)
+            {
+                j++; // Move to the next character in str2
+            }
+
+            i++; // Always move to the next character in str1
+        }
+
+        return j == m;
+    }
+}
--- LeetCode/Coding_challenge_12_2024/README.md
+++ LeetCode/Coding_challenge_12_2024/README.md
@@ -6,19 +6,28 @@
     <th>Name</th>
     <th>Link solution</th>
   </tr>
+
   <tr>
     <td>1</td>
     <td>Check If N and Its Double Exist</td>
     <td><a href="https://leetcode.com/problems/check-if-n-and-its-double-exist/submissions/1468175143">link</a></td>
   </tr>
+
   <tr>
     <td>2</td>
     <td>Check If a Word Occurs As a Prefix of Any Word in a Sentence</td>
     <td><a href="https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/submissions/1467908555">link</a></td>
   </tr>
+  
   <tr>
     <td>3</td>
     <td>Adding Spaces to a String</td>
     <td><a href="https://leetcode.com/problems/adding-spaces-to-a-string/submissions/1468821801">link</a></td>
+  </tr>
+
+  <tr>
+    <td>4</td>
+    <td>Make String a Subsequence Using Cyclic Increments</td>
+    <td><a href="https://leetcode.com/problems/make-string-a-subsequence-using-cyclic-increments/submissions/1470038997">link</a></td>
   </tr>
 </table>
