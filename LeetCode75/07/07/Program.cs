08d37caa27d9cbf29be0a5b07e78c83d8f786018
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-16 16:57:13 +0700

    Leetcode 75 questions no.07

---
+++ LeetCode75/07/07/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([1, 2, 3, 4], [24, 12, 8, 6], "Test case 1");
+    }
+
+    public static void TestCase(int[] nums, int[] expectation, string description)
+    {
+        var result = ProductExceptSelf(nums);
+        string resultString = string.Join(", ", result);
+        string expectationString = string.Join(", ", expectation);
+        Console.WriteLine($"{description} {(resultString == expectationString ? "Passed" : "Failed")}");
+    }
+
+    public static int[] ProductExceptSelf(int[] nums)
+    {
+        var n = nums.Length;
+        var output = new int[n];
+        output[0] = 1;
+        for (var i = 1; i < n; i++)
+        {
+            output[i] = output[i - 1] * nums[i - 1];
+        }
+
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            output[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return output;
+    }
+}
---
+++ LeetCode75/07/README.md
@@ -0,0 +1,11 @@
+# **Reverse Words in a String**
+
+Given an integer array `nums`, return an array `answer` such that `answer[i]` is equal to the product of all the elements of `nums` except `nums[i]`.
+
+The product of any prefix or suffix of `nums` is **guaranteed** to fit in a **32-bit** integer.
+
+You must write an algorithm that runs in `O(n)` time and without using the division operation.
+
+View full [question](https://leetcode.com/problems/product-of-array-except-self?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/product-of-array-except-self/submissions/1480115737)
