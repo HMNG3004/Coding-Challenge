4dc74b1a09fc1fb406c28bb1e1cdaff0c027268e
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-16 10:30:57 +0700

    16-12-2024

---
+++ LeetCode/Coding_challenge_12_2024/16-12-2024/16-12-2024/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([2, 1, 3, 5, 6], 5, 2, [8, 4, 6, 5, 6], "Test case 1");
+    }
+
+    public static void TestCase(int[] nums, int k, int multiplier, int[] expectation, string description)
+    {
+        var result = GetFinalState(nums, k, multiplier);
+        var hasDuplicates = result.Intersect(expectation).Any();
+        Console.WriteLine($"{description} {(hasDuplicates ? "Passed" : "Failed")}");
+    }
+
+    public static int[] GetFinalState(int[] nums, int k, int multiplier)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            var min = int.MaxValue;
+            var minIndex = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] < min)
+                {
+                    min = nums[j];
+                    minIndex = j;
+                }
+            }
+
+            nums[minIndex] *= multiplier;
+        }
+        return nums;
+    }
+}
---
+++ LeetCode/Coding_challenge_12_2024/16-12-2024/README.md
@@ -0,0 +1,14 @@
+# **Final Array State After K Multiplication Operations I**
+
+You are given an integer array `nums`, an integer `k`, and an integer `multiplier`.
+
+You need to perform `k` operations on `nums`. In each operation:
+
+- Find the **minimum** value `x` in `nums`. If there are multiple occurrences of the minimum value, select the one that appears **first**.
+- Replace the selected minimum value `x` with `x * multiplie`r.
+
+_Return an integer array denoting the final state of `nums` after performing all `k` operations._
+
+View full question <a href="https://leetcode.com/problems/final-array-state-after-k-multiplication-operations-i?envType=daily-question&envId=2024-12-16">here</a>
+
+Here is my [submission](https://leetcode.com/problems/final-array-state-after-k-multiplication-operations-i/submissions/1479840319)
