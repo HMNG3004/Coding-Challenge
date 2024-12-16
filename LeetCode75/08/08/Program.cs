702df4d69e8c0bf8dbda6fa6b2069ab0877c8d5d
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-16 17:09:27 +0700

    Leetcode 75 questions no.08

---
+++ LeetCode75/08/08/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([1, 2, 3, 4, 5], true, "Test Case 1");
+        TestCase([5, 4, 3, 2, 1], false, "Test Case 2");
+        TestCase([2, 1, 5, 0, 4, 6], true, "Test Case 3");
+    }
+
+    public static void TestCase(int[] nums, bool expectation, string description)
+    {
+        var result = IncreasingTriplet(nums);
+        Console.WriteLine($"{description} : {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static bool IncreasingTriplet(int[] nums)
+    {
+        if (nums.Length < 3)
+            return false;
+
+        int first = int.MaxValue;
+        int second = int.MaxValue;
+
+        foreach (int num in nums)
+        {
+            if (num <= first)
+            {
+                first = num;
+            }
+            else if (num <= second)
+            {
+                second = num;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ LeetCode75/08/README.md
@@ -0,0 +1,7 @@
+# **Reverse Words in a String**
+
+Given an integer array `nums`, return `true` if there exists a triple of indices `(i, j, k)` such that `i < j < k` and `nums[i] < nums[j] < nums[k]`. If no such indices exists, return `false`.
+
+View full [question](https://leetcode.com/problems/increasing-triplet-subsequence?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/increasing-triplet-subsequence/submissions/1480124342)
