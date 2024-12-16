bf98eb8a5bc96dff2fedfe0de30aa07e29314ff8
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-16 15:31:15 +0700

    Leetcode 75 questions no.04

---
+++ LeetCode75/04/04/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([1, 0, 0, 0, 1], 1, true, "Test case 1");
+        TestCase([1, 0, 0, 0, 1], 2, false, "Test case 2");
+        TestCase([0], 1, true, "Test case 3");
+    }
+
+    public static void TestCase(int[] flowerbed, int n, bool expectation, string desciption)
+    {
+        var result = CanPlaceFlowers(flowerbed, n);
+        System.Console.WriteLine($"{desciption} {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        for (int i = 0; i < flowerbed.Length; i++)
+        {
+            int min = i - 1 < 0 ? 0 : i - 1;
+            int max = i + 1 >= flowerbed.Length ? flowerbed.Length - 1 : i + 1;
+
+            if (flowerbed[min] == 0 && flowerbed[i] == 0 && flowerbed[max] == 0)
+            {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n <= 0;
+    }
+}
---
+++ LeetCode75/04/README.md
@@ -0,0 +1,9 @@
+# **Can Place Flowers**
+
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in **adjacent** plots.
+
+Given an integer array `flowerbed` containing `0`'s and `1`'s, where `0` means empty and `1` means not empty, and an integer `n`, return _`true` if `n` new flowers can be planted in the `flowerbed` without violating the no-adjacent-flowers rule and `false` otherwise_.
+
+View full [question](https://leetcode.com/problems/can-place-flowers?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/can-place-flowers/submissions/1480058079)
