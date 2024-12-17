815e1806990fe00d9053362091f86793975e322e
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-18 00:59:02 +0700

    Leetcode 75 questions no.11

---
+++ LeetCode75/11/11/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase("abc", "ahbgdc", true, "Test case 1");
+        TestCase("axc", "ahbgdc", false, "Test case 1");
+    }
+
+    public static void TestCase(string s, string t, bool expectation, string description)
+    {
+        var result = IsSubsequence(s, t);
+        Console.WriteLine($"{description} : {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static bool IsSubsequence(string s, string t)
+    {
+        return IsSubsequence(s, t, s.Length, t.Length);
+    }
+
+    public static bool IsSubsequence(string s1, string s2, int m, int n)
+    {
+        if (m == 0)
+            return true;
+        if (n == 0)
+            return false;
+
+        // If last characters of two strings are matching
+        if (s1[m - 1] == s2[n - 1])
+            return IsSubsequence(s1, s2, m - 1, n - 1);
+
+        return IsSubsequence(s1, s2, m, n - 1);
+    }
+}
---
+++ LeetCode75/11/README.md
@@ -0,0 +1,9 @@
+# **Move Zeroes**
+
+Given two strings `s` and `t`, return `true` if `s` is a **subsequence** _of_ `t`, _or_ `false` otherwise.
+
+A **subsequence** of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., `"ace"` is a subsequence of `"abcde"` while `"aec"` is not).
+
+View full [question](https://leetcode.com/problems/is-subsequence?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/is-subsequence/submissions/1481351117)
