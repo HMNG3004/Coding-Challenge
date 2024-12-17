f4f444d4ec99fbeaedee8d525c86cb7e62b38043
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-18 01:16:49 +0700

    Leetcode 75 questions no.12

---
+++ LeetCode75/12/12/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([1, 8, 6, 2, 5, 4, 8, 3, 7], 49, "Test Case 1");
+        TestCase([1, 1], 1, "Test Case 2");
+    }
+
+    public static void TestCase(int[] height, int expectation, string description)
+    {
+        var result = MaxArea(height);
+        Console.WriteLine($"{description} : {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int MaxArea(int[] height)
+    {
+        int left = 0;
+        int right = height.Length - 1;
+        int maxArea = 0; int minHeight = 0;
+        while (left < right)
+        {
+            minHeight = Math.Min(height[left], height[right]);
+            maxArea = Math.Max(maxArea, minHeight * (right - left));
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
---
+++ LeetCode75/12/README.md
@@ -0,0 +1,13 @@
+# **Container With Most Water**
+
+You are given an integer array `height` of length `n`. There are `n` vertical lines drawn such that the two endpoints of the **i<sup>th</sup>** line are `(i, 0)` and `(i, height[i])`.
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+_Return the maximum amount of water a container can store._
+
+**Notice** that you may not slant the container.
+
+View full [question](https://leetcode.com/problems/container-with-most-water?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/container-with-most-water/submissions/1481365948)
