c80bfbad51255edb6c8f3d83907a75c0a99f9a39
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-18 00:38:39 +0700

    Leetcode 75 questions no.10

---
+++ LeetCode75/10/10/Program.cs
@@ -0,0 +1,53 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+
+    }
+
+    public static void MoveZeroes(int[] nums)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+        while (left < right)
+        {
+            if (nums[left] == 0)
+            {
+                for (int i = left; i < right; i++)
+                {
+                    nums[i] = nums[i + 1];
+                }
+                nums[right] = 0;
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+    }
+
+    public static void MoveZeroes2(int[] nums)
+    {
+        int i = 0;
+        int j = 0;
+        while (i < nums.Length)
+        {
+            if (nums[i] == 0)
+            {
+                i++;
+            }
+            else
+            {
+                nums[j] = nums[i];
+                i++;
+                j++;
+            }
+        }
+        while (j < nums.Length)
+        {
+            nums[j] = 0;
+            j++;
+        }
+    }
+}
---
+++ LeetCode75/10/README.md
@@ -0,0 +1,9 @@
+# **Move Zeroes**
+
+Given an integer array `nums`, move all `0`'s to the end of it while maintaining the relative order of the non-zero elements.
+
+**Note** that you must do this in-place without making a copy of the array.
+
+View full [question](https://leetcode.com/problems/move-zeroes?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/move-zeroes/submissions/1481328133)
