4fe96a25b5db613c053875f753745965cd85bcc6
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-18 00:20:22 +0700

    17-12-2024

---
+++ LeetCode/Coding_challenge_12_2024/17-12-2024/17-12-2024/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        TestCase("cczazcc", 3, "zzcccac", "Test case 1");
+        TestCase("aababab", 2, "bbabaa", "Test case 2");
+    }
+
+    public static void TestCase(string s, int repeatLimit, string expectation, string description)
+    {
+        var result = RepeatLimitedString(s, repeatLimit);
+        Console.WriteLine($"{description} : {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static string RepeatLimitedString(string s, int repeatLimit)
+    {
+        int[] freq = new int[26];
+        foreach (char c in s)
+            freq[c - 'a']++;
+
+        var result = new StringBuilder();
+        int i = 25;
+        while (true)
+        {
+            while (i >= 0 && freq[i] == 0) i--;
+            if (i < 0) break;
+
+            int count = Math.Min(freq[i], repeatLimit);
+            result.Append((char)(i + 'a'), count);
+            freq[i] -= count;
+
+            if (freq[i] > 0)
+            {
+                int j = i - 1;
+                while (j >= 0 && freq[j] == 0) j--;
+
+                if (j < 0) break;
+
+                result.Append((char)(j + 'a'));
+                freq[j]--;
+            }
+        }
+
+        return result.ToString();
+    }
+}
---
+++ LeetCode/Coding_challenge_12_2024/17-12-2024/README.md
@@ -0,0 +1,11 @@
+# **Final Array State After K Multiplication Operations I**
+
+You are given a string `s` and an integer `repeatLimit`. Construct a new string `repeatLimitedString` using the characters of `s` such that no letter appears **more than** `repeatLimit` times `in a row`. You do not have to use all characters from `s`.
+
+_Return the *lexicographically largest*_ `repeatLimitedString` _possible._
+
+A string `a` is _lexicographically larger_ than a string `b` if in the first position where `a` and `b` differ, string `a` has a letter that appears later in the alphabet than the corresponding letter in `b`. If the first `min(a.length, b.length)` characters do not differ, then the longer string is the lexicographically larger one.
+
+View full [question](https://leetcode.com/problems/construct-string-with-repeat-limit?envType=daily-question&envId=2024-12-17)
+
+View my [submission](https://leetcode.com/problems/construct-string-with-repeat-limit/submissions/1481257065)
