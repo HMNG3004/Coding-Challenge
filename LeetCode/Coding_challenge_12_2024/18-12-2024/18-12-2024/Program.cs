cd4b19a14ca1bb51b97a18b876d317d5b925cdea
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-18 14:28:43 +0700

    18-12-2024

---
+++ LeetCode/Coding_challenge_12_2024/18-12-2024/18-12-2024/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([8, 4, 6, 2, 3], [4, 2, 4, 2, 3], "Test case 1");
+        TestCase([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], "Test case 2");
+        TestCase([10, 1, 1, 6], [9, 0, 1, 6], "Test case 3");
+    }
+
+    public static void TestCase(int[] prices, int[] expectation, string description)
+    {
+        var result = FinalPrices(prices);
+        var resultString = string.Join(",", result);
+        var expectationString = string.Join(",", expectation);
+        Console.WriteLine($"{description} : {(expectationString == resultString ? "Passed" : "Failed")}");
+    }
+
+    public static int[] FinalPrices(int[] prices)
+    {
+        for (int i = 0; i < prices.Length; i++)
+        {
+            for(int j = i + 1; j < prices.Length; j++)
+            {
+                if (prices[j] <= prices[i])
+                {
+                    prices[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+}
---
+++ LeetCode/Coding_challenge_12_2024/18-12-2024/README.md
@@ -0,0 +1,11 @@
+# **Final Prices With a Special Discount in a Shop**
+
+You are given an integer array `prices` where `prices[i]` is the price of the **i<sup>th</sup>** item in a shop.
+
+There is a special discount for items in the shop. If you buy the **i<sup>th</sup>** item, then you will receive a discount equivalent to `prices[j]` where `j` is the minimum index such that `j > i` and `prices[j] <= prices[i]`. Otherwise, you will not receive any discount at all.
+
+Return an integer array `answer` where `answer[i]` is the final price you will pay for the **i<sup>th</sup>** item of the shop, considering the special discount.
+
+View full [question](https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop?envType=daily-question&envId=2024-12-18)
+
+View my [submission](https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/submissions/1481785518)
