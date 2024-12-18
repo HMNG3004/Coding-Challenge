cdd291147b693539778bf5702f629f2a2be20edc
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-18 15:48:59 +0700

    Leetcode 75 questions no.14

---
+++ LeetCode75/14/14/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([0, 4, 0, 3, 2], 1, 4.00000, "Test Case 1");
+        TestCase([5], 1, 5.00000, "Test Case 2");
+    }
+
+    public static void TestCase(int[] nums, int k, double expectation, string description)
+    {
+        var result = FindMaxAverage(nums, k);
+        result = Math.Round(result, 5);
+        Console.WriteLine($"{description} : {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static double FindMaxAverage(int[] nums, int k)
+    {
+        double max = 0;
+        for (int i = 0; i < k; i++)
+        {
+            max += nums[i];
+        }
+        double window_sum = max;
+        for (int i = k; i < nums.Length; i++)
+        {
+            window_sum += nums[i] - nums[i - k];
+            max = Math.Max(max, window_sum);
+        }
+
+        return max / k;
+    }
+}
---
+++ LeetCode75/14/README.md
@@ -0,0 +1,9 @@
+# **Maximum Average Subarray I**
+
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+View full [question](https://leetcode.com/problems/maximum-average-subarray-i?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/maximum-average-subarray-i/submissions/1481848764)
