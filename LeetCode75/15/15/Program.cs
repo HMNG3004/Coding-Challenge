a24c07bce00c44e830223f4710e7ba191391c00f
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-18 16:43:40 +0700

    Leetcode 75 questions no.15

---
+++ LeetCode75/15/15/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase("abciiidef", 3, 3, "Test Case 1");
+    }
+
+    public static void TestCase(string s, int k, int expectation, string description)
+    {
+        var result = MaxVowels(s, k);
+        Console.WriteLine($"{description} : {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int MaxVowels(string s, int k)
+    {
+        int max = 0;
+        char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+        for(int i = 0; i < k; i++)
+        {
+            if (vowels.Contains(s[i]))
+                max++;
+        }
+        int count = max;
+        for(int i = k; i < s.Length; i++)
+        {
+            if (vowels.Contains(s[i]))
+                count++;
+
+            //Check if the previous character was a vowel
+            if (vowels.Contains(s[i - k]))
+                count--;
+            max = Math.Max(max, count);
+        }
+        return max;
+    }
+}
---
+++ LeetCode75/15/README.md
@@ -0,0 +1,9 @@
+# **Maximum Number of Vowels in a Substring of Given Length**
+
+Given a string `s` and an integer `k`, return the maximum number of vowel letters in any substring of `s` with length `k`.
+
+Vowel letters in English are `'a'`, `'e'`, `'i'`, `'o'`, and `'u'`.
+
+View full [question](https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/submissions/1481891161)
