8e0e405cea10f4319b66e5268833d50e6b1423cf
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-19 11:27:44 +0700

    19-12-2024

---
+++ LeetCode/Coding_challenge_12_2024/19-12-2024/19-12-2024/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([1, 0, 2, 3, 4], 4, "Test Case 1");
+        TestCase([4, 3, 2, 1, 0], 1, "Test Case 2");
+    }
+
+    public static void TestCase(int[] arr, int expectation, string description)
+    {
+        var result = MaxChunksToSorted(arr);
+        Console.WriteLine($"{description} : {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int MaxChunksToSorted(int[] arr)
+    {
+        int n = arr.Length;
+        int[] max = arr.Clone() as int[];
+        int[] min = arr.Clone() as int[];
+
+        for(int i = 1; i < n; i++)
+        {
+            max[i] = Math.Max(max[i - 1], max[i]);
+        }
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            min[i] = Math.Min(min[i + 1], min[i]);
+        }
+
+        int chunks = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0 || min[i] > max[i - 1])
+            {
+                chunks++;
+            }
+        }
+
+        return chunks;
+    }
+}
---
+++ LeetCode/Coding_challenge_12_2024/19-12-2024/README.md
@@ -0,0 +1,11 @@
+# **Max Chunks To Make Sorted**
+
+You are given an integer array `arr` of length `n` that represents a permutation of the integers in the range `[0, n - 1]`.
+
+We split `arr` into some number of **chunks** (i.e., partitions), and individually sort each chunk. After concatenating them, the result should equal the sorted array.
+
+_Return the largest number of chunks we can make to sort the array._
+
+View full [question](https://leetcode.com/problems/max-chunks-to-make-sorted?envType=daily-question&envId=2024-12-19)
+
+View my [submission](https://leetcode.com/problems/max-chunks-to-make-sorted/submissions/1482554932)
