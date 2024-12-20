990de4cdbed39beb1810394981f47983f8220c9c
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-20 23:48:29 +0700

    LeetCode 75 questions no.16

---
+++ LeetCode75/16/16/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2, 6, "Test case 1");
+        TestCase([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3, 10, "Test case 2");
+    }
+
+    public static void TestCase(int[] nums, int k, int expectation, string description)
+    {
+        var result = LongestOnes(nums, k);
+        Console.WriteLine($"{description} : {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int LongestOnes(int[] nums, int k)
+    {
+        int left = 0;
+        int maxLength = 0;
+        int zeroCount = 0;
+
+        for(int right = 0; right < nums.Length; right++)
+        {
+            if (nums[right] == 0)
+            {
+                zeroCount++;
+            }
+
+            while(zeroCount > k)
+            {
+                if (nums[left] == 0)
+                {
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxLength = Math.Max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
---
+++ LeetCode75/16/README.md
@@ -0,0 +1,7 @@
+# **Max Consecutive Ones III**
+
+_Given a binary array `nums` and an integer `k`, return the maximum number of consecutive `1`'s in the array if you can flip at most `k` `0`'s._
+
+View full [question](https://leetcode.com/problems/max-consecutive-ones-iii?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/max-consecutive-ones-iii/submissions/1482591994)
