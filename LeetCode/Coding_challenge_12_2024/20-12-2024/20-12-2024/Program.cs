2dfc937c7f5a74828139657a4376a7d2c47e5395
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-20 23:41:36 +0700

    20-12-2024

---
+++ LeetCode/Coding_challenge_12_2024/20-12-2024/20-12-2024/Program.cs
@@ -0,0 +1,44 @@
+﻿public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+
+    public TreeNode ReverseOddLevels(TreeNode root)
+    {
+        Reverse(root.left, root.right, 0);
+        return root;
+    }
+
+    private void Reverse(TreeNode leftChild, TreeNode rightChild, int level)
+    {
+        if (leftChild == null || rightChild == null)
+        {
+            return;
+        }
+
+        if (level % 2 == 0)
+        {
+            int temp = leftChild.val;
+            leftChild.val = rightChild.val;
+            rightChild.val = temp;
+        }
+
+        Reverse(leftChild.left, rightChild.right, level + 1);
+        Reverse(leftChild.right, rightChild.left, level + 1);
+    }
+}
---
+++ LeetCode/Coding_challenge_12_2024/20-12-2024/README.md
@@ -0,0 +1,15 @@
+# **Max Chunks To Make Sorted**
+
+Given the `root` of a **perfect** binary tree, reverse the node values at each odd level of the tree.
+
+- For example, suppose the node values at level 3 are `[2,1,3,4,7,11,29,18]`, then it should become `[18,29,11,7,4,3,1,2]`.
+
+_Return the root of the reversed tree._
+
+A binary tree is **perfect** if all parent nodes have two children and all leaves are on the same level.
+
+The **level** of a node is the number of edges along the path between it and the root node.
+
+View full [question](https://leetcode.com/problems/reverse-odd-levels-of-binary-tree?envType=daily-question&envId=2024-12-20)
+
+View my [submission](https://leetcode.com/problems/reverse-odd-levels-of-binary-tree/submissions/1483877829)
