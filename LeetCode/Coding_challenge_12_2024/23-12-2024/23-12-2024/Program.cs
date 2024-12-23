b73f3fe5fb37531b86822d8156844d2ad07862be
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-23 15:05:22 +0700

    23-12-2024

---
+++ LeetCode/Coding_challenge_12_2024/23-12-2024/23-12-2024/Program.cs
@@ -0,0 +1,94 @@
+﻿public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+
+    public static void TestCase()
+    {
+
+    }
+
+    public static int MinimumOperations(TreeNode root)
+    {
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        int totalSwaps = 0;
+
+        while (queue.Count > 0)
+        {
+            int levelSize = queue.Count;
+            var currentLevel = new List<int>();
+
+            for (int i = 0; i < levelSize; i++)
+            {
+                var node = queue.Dequeue();
+                currentLevel.Add(node.val);
+
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+
+            totalSwaps += MinSwapsToSort(currentLevel);
+        }
+
+        return totalSwaps;
+    }
+
+    public static int MinSwapsToSort(List<int> arr)
+    {
+        int n = arr.Count;
+        var indexedArr = new List<(int value, int index)>();
+        for (int i = 0; i < n; i++)
+        {
+            indexedArr.Add((arr[i], i));
+        }
+
+        indexedArr.Sort((a, b) => a.value.CompareTo(b.value));
+        var visited = new bool[n];
+        int swaps = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (visited[i] || indexedArr[i].index == i)
+                continue;
+
+            int cycleSize = 0;
+            int x = i;
+
+            while (!visited[x])
+            {
+                visited[x] = true;
+                x = indexedArr[x].index;
+                cycleSize++;
+            }
+
+            if (cycleSize > 1)
+            {
+                swaps += cycleSize - 1;
+            }
+        }
+
+        return swaps;
+    }
+}
---
+++ LeetCode/Coding_challenge_12_2024/23-12-2024/README.md
@@ -0,0 +1,13 @@
+# **Minimum Number of Operations to Sort a Binary Tree by Level**
+
+You are given the `root` of a binary tree with **unique values**.
+
+In one operation, you can choose any two nodes at the same level and swap their values.
+
+_Return the minimum number of operations needed to make the values at each level sorted in a **strictly increasing order**._
+
+The **level** of a node is the number of edges along the path between it and the root node.
+
+View full [question](https://leetcode.com/problems/minimum-number-of-operations-to-sort-a-binary-tree-by-level?envType=daily-question&envId=2024-12-23)
+
+View my [submission](https://leetcode.com/problems/minimum-number-of-operations-to-sort-a-binary-tree-by-level/submissions/1486084474)
