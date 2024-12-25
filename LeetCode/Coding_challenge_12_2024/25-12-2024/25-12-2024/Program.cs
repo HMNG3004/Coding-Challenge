c72317777a3b3dea6d0ecf562dd1cd6af95e5082
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-25 11:19:34 +0700

    25-12-2024

---
+++ LeetCode/Coding_challenge_12_2024/25-12-2024/25-12-2024/Program.cs
@@ -0,0 +1,43 @@
+﻿public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Program
+{
+    public IList<int> LargestValues(TreeNode root)
+    {
+        var result = new List<int>();
+        if (root == null) return result;
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            int levelSize = queue.Count;
+            int maxInRow = int.MinValue;
+
+            for (int i = 0; i < levelSize; i++)
+            {
+                var node = queue.Dequeue();
+                maxInRow = Math.Max(maxInRow, node.val);
+
+                if (node.left != null) queue.Enqueue(node.left);
+                if (node.right != null) queue.Enqueue(node.right);
+            }
+
+            result.Add(maxInRow);
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/Coding_challenge_12_2024/25-12-2024/README.md
@@ -0,0 +1,7 @@
+# **Find Largest Value in Each Tree Row**
+
+Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
+
+View full [question](https://leetcode.com/problems/find-largest-value-in-each-tree-row?envType=daily-question&envId=2024-12-25)
+
+View my [submission](https://leetcode.com/problems/find-largest-value-in-each-tree-row/submissions/1487650199)
