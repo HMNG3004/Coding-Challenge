e1903752683a1c3dda0cc639654d257abc4a39fd
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-28 00:10:21 +0700

    27-12-2024

---
+++ LeetCode/Coding_challenge_12_2024/27-12-2024/27-12-2024/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([8, 1, 5, 2, 6], 11, "Test case 1");
+        TestCase([1, 2], 2, "Test case 2");
+    }
+
+    public static void TestCase(int[] values, int expectation, string description)
+    {
+        var result = MaxScoreSightseeingPair(values);
+        Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int MaxScoreSightseeingPair(int[] values)
+    {
+        int[] maxLeftScore = new int[values.Length];
+        maxLeftScore[0] = values[0];
+
+        int maxScore = 0;
+
+        for (int i = 1; i < values.Length; i++)
+        {
+            int currentRightScore = values[i] - i;
+            maxScore = Math.Max(
+                maxScore,
+                maxLeftScore[i - 1] + currentRightScore
+            );
+
+            int currentLeftScore = values[i] + i;
+            maxLeftScore[i] = Math.Max(maxLeftScore[i - 1], currentLeftScore);
+        }
+
+        return maxScore;
+    }
+}
---
+++ LeetCode/Coding_challenge_12_2024/27-12-2024/README.md
@@ -0,0 +1,11 @@
+# **Best Sightseeing Pair**
+
+You are given an integer array `values` where values[i] represents the value of the **i<sup>th</sup>** sightseeing spot. Two sightseeing spots `i` and `j` have a *distance* `j - i` between them.
+
+The score of a pair (`i < j`) of sightseeing spots is `values[i] + values[j] + i - j`: the sum of the values of the sightseeing spots, minus the distance between them.
+
+_Return the maximum score of a pair of sightseeing spots._
+
+View full [question](https://leetcode.com/problems/best-sightseeing-pair?envType=daily-question&envId=2024-12-27)
+
+View my [submission](https://leetcode.com/problems/best-sightseeing-pair/submissions/1489967547)
