94b4b581b5e394c8e532be96e2962a2af38f5d0e
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-29 00:29:15 +0700

    Leetcode 75 questions no.18

---
+++ LeetCode75/03_Sliding window/17/README.md
@@ -0,0 +1,9 @@
+# **Longest Subarray of 1's After Deleting One Element**
+
+Given a binary array `nums`, you should delete one element from it.
+
+_Return the size of the longest non-empty subarray containing only `1`'s in the resulting array. Return `0` if there is no such subarray._
+
+View full [question](https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/submissions/1484787557)
---
+++ LeetCode75/04_Prefix sum/18/18/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([-5, 1, 5, 0, -7], 1, "Test case 1");
+        TestCase([-4, -3, -2, -1, 4, 3, 2], 0, "Test case 2");
+    }
+
+    public static void TestCase(int[] gain, int expectation, string description)
+    {
+        var result = LargestAltitude(gain);
+        System.Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int LargestAltitude(int[] gain)
+    {
+        int highest = 0;
+        int[] altitude = new int[gain.Length + 1];
+        altitude[0] = 0;
+        for(int i = 0; i < gain.Length; i++)
+        {
+            altitude[i + 1] = altitude[i] + gain[i];
+            highest = Math.Max(highest, altitude[i + 1]);
+        }
+
+        return highest;
+    }
+}
