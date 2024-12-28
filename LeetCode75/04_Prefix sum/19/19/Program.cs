4f4bf3cb821530e6d51811817363e7ab2fc221b6
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-29 01:46:49 +0700

    Leetcode 75 questions no.19

---
+++ LeetCode75/04_Prefix sum/19/19/Program.cs
@@ -0,0 +1,65 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([1, 7, 3, 6, 5, 6], 3, "Test case 1");
+        TestCase([1, 2, 3], -1, "Test case 2");
+        TestCase([2, 1, -1], 0, "Test case 3");
+    }
+
+    public static void TestCase(int[] nums, int expectation, string description)
+    {
+        var result = PivotIndex2(nums);
+        Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int PivotIndex(int[] nums)
+    {
+        int result = -1;
+        int start = 0;
+        int[] prefixSum = new int[nums.Length + 1];
+        int[] suffixSum = new int[nums.Length + 1];
+        prefixSum[0] = 0;
+        suffixSum[nums.Length - 1] = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+            suffixSum[nums.Length - 1 - i] = suffixSum[nums.Length - i] + nums[nums.Length - 1 - i];
+        }
+
+        while (start < suffixSum.Length - 1)
+        {
+            if (prefixSum[start] == suffixSum[start + 1])
+            {
+                result = start;
+                break;
+            }
+            start++;
+        }
+
+        return result;
+    }
+
+    public static int PivotIndex2(int[] nums)
+    {
+        int totalSum = 0;
+        int leftSum = 0;
+
+        foreach (int num in nums)
+        {
+            totalSum += num;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (leftSum == totalSum - leftSum - nums[i])
+            {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
---
+++ LeetCode75/04_Prefix sum/19/README.md
@@ -0,0 +1,13 @@
+# **Find Pivot Index**
+
+Given an array of integers `nums`, calculate the **pivot index** of this array.
+
+The **pivot index** is the index where the sum of all the numbers **strictly** to the left of the index is equal to the sum of all the numbers **strictly** to the index's right.
+
+If the index is on the left edge of the array, then the left sum is `0` because there are no elements to the left. This also applies to the right edge of the array.
+
+_Return the **leftmost pivot index**._ If no such index exists, return `-1`.
+
+View full [question](https://leetcode.com/problems/find-pivot-index?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/find-pivot-index/submissions/1490897017)
