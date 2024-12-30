365fa500be7686afea735a0a7eeb23d3c2233d5d
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-30 14:22:25 +0700

    Leetcode 75 questions no.20

---
+++ LeetCode75/05_Hash Map-Set/20/20/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([1, 2, 3], [2, 4, 6], "Test case 1:");
+        TestCase([1, 2, 3, 3], [1, 1, 2, 2], "Test case 2:");
+    }
+
+    public static void TestCase(int[] nums1, int[] nums, string description)
+    {
+        Console.WriteLine(description);
+        var result = FindDifference(nums1, nums);
+        foreach (var item in result)
+        {
+            Console.WriteLine(string.Join(",", item));
+        }
+    }
+
+    public static IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+    {
+        var result = new List<IList<int>>();
+        var listNums1 = Different(nums2, nums1);
+        var listNums2 = Different(nums1, nums2);
+        result.Add(listNums1);
+        result.Add(listNums2);
+
+        return result;
+    }
+
+    public static List<int> Different(int[] source, int[] target)
+    {
+        var targetSet = new HashSet<int>(target);
+
+        foreach (var num in source)
+        {
+            if (targetSet.Contains(num))
+            {
+                targetSet.Remove(num);
+            }
+        }
+
+        return targetSet.ToList();
+    }
+}
---
+++ LeetCode75/05_Hash Map-Set/20/README.md
@@ -0,0 +1,12 @@
+# **Find the Difference of Two Arrays**
+
+Given two **0-indexed** integer arrays `nums1` and `nums2`, return _a list `answer` of size `2` where:_
+
+- `answer[0]` _is a list of all **distinct** integers in `nums1` which are **not** present in `nums2`._
+- `answer[1]` _is a list of all **distinct** integers in `nums2` which are **not** present in `nums1`._
+
+**Note** that the integers in the lists may be returned in **any** order.
+
+View full [question](https://leetcode.com/problems/find-the-difference-of-two-arrays?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/find-the-difference-of-two-arrays/submissions/1492213356)
