9a1caef6a7fd74b13c4a3cef52f1a12124982bfa
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-30 13:16:16 +0700

    30-12-2024

---
+++ LeetCode/Coding_challenge_12_2024/30-12-2024/30-12-2024/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase(3, 3, 1, 1, 8, "Test case 1");
+        TestCase(2, 3, 1, 2, 5, "Test case 2");
+    }
+
+    public static void TestCase(int low, int high, int zero, int one, int expectation, string description)
+    {
+        var result = CountGoodStrings(low, high, zero, one);
+        Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int CountGoodStrings(int low, int high, int zero, int one)
+    {
+        const int Mod = 1_000_000_007;
+        var dp = new int[high + 1];
+        dp[0] = 1;
+
+        var result = 0;
+
+        for (var i = 1; i <= high; ++i)
+        {
+            if (i >= zero)
+            {
+                dp[i] = (dp[i] + dp[i - zero]) % Mod;
+            }
+
+            if (i >= one)
+            {
+                dp[i] = (dp[i] + dp[i - one]) % Mod;
+            }
+
+            if (i >= low)
+            {
+                result = (result + dp[i]) % Mod;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/Coding_challenge_12_2024/30-12-2024/README.md
@@ -0,0 +1,16 @@
+# **Count Ways To Build Good Strings**
+
+Given the integers `zero`, `one`, `low`, and `high`, we can construct a string by starting with an empty string, and then at each step perform either of the following:
+
+- Append the character `'0'` `zero` times.
+- Append the character `'1'` `one` times.
+
+This can be performed any number of times.
+
+A **good** string is a string constructed by the above process having a **length** between `low` and `high` (**inclusive**).
+
+_Return the number of **different** good strings that can be constructed satisfying these properties._ Since the answer can be large, return it **modulo** **10<sup>9</sup> + 7**.
+
+View full [question](https://leetcode.com/problems/count-ways-to-build-good-strings?envType=daily-question&envId=2024-12-30)
+
+View my [submission](https://leetcode.com/problems/count-ways-to-build-good-strings/submissions/1492149232)
