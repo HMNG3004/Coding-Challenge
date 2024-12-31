524443bf64b9ec0155ba609c99710ee4aed65f9b
Hoang Minh <hoangminh30042003@gmail.com>
2024-12-31 14:31:42 +0700

    Leetcode 75 questions no.21

---
+++ LeetCode75/05_Hash Map-Set/21/21/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([1, 2, 2, 1, 1, 3], true, "Test case 1");
+        TestCase([1, 2], false, "Test case 2");
+        TestCase([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0], true, "Test case 3");
+    }
+
+    public static void TestCase(int[] arr, bool expectation, string description)
+    {
+        var result = UniqueOccurrences(arr);
+        Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static bool UniqueOccurrences(int[] arr)
+    {
+        var occur = new Dictionary<int, int>();
+        foreach (var item in arr)
+        {
+            if (occur.ContainsKey(item))
+            {
+                occur[item]++;
+            }
+            else
+            {
+                occur.Add(item, 1);
+            }
+        }
+
+        var set = new HashSet<int>();
+        foreach (var count in occur)
+        {
+            if (!set.Add(count.Value)) return false;
+        }
+        return true;
+    }
+}
---
+++ LeetCode75/05_Hash Map-Set/21/README.md
@@ -0,0 +1,7 @@
+# **Unique Number of Occurrences**
+
+Given an array of integers `arr`, return `true` if the number of occurrences of each value in the array is **unique** or `false` otherwise.
+
+View full [question](https://leetcode.com/problems/unique-number-of-occurrences?envType=study-plan-v2&envId=leetcode-75)
+
+View my [submission](https://leetcode.com/problems/unique-number-of-occurrences/submissions/1493142669)
