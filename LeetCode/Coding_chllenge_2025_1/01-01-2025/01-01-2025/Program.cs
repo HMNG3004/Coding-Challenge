6d9cee879234da6fc4eaa3bf7585b7bb46c6ba78
Hoang Minh <hoangminh30042003@gmail.com>
2025-01-01 15:30:50 +0700

    01-01-2025

---
+++ LeetCode/Coding_chllenge_2025_1/01-01-2025/01-01-2025/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase("011101", 5, "Test case 1");
+        TestCase("00111", 5, "Test case 2");
+        TestCase("1111", 3, "Test case 3");
+    }
+
+    public static void TestCase(string s, int expectation, string description)
+    {
+        var result = MaxScore(s);
+        Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int MaxScore(string s)
+    {
+        int max = 0;
+        int countOne = 0, countZero = 0;
+        foreach (char c in s)
+        {
+            if (c == '1')
+            {
+                countOne++;
+            }
+        }
+
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            if (s[i] == '0')
+            {
+                countZero++;
+            }
+            else
+            {
+                countOne--;
+            }
+            max = Math.Max(max, countOne + countZero);
+        }
+
+        return max;
+    }
+}
---
+++ LeetCode/Coding_chllenge_2025_1/01-01-2025/README.md
@@ -0,0 +1,9 @@
+# **Maximum Score After Splitting a String**
+
+Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
+
+The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
+
+View full [question](https://leetcode.com/problems/maximum-score-after-splitting-a-string?envType=daily-question&envId=2025-01-01)
+
+View my [submission](https://leetcode.com/problems/maximum-score-after-splitting-a-string/submissions/1493843336)
