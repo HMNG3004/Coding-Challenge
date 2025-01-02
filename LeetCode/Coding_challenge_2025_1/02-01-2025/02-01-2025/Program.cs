eff949a6c390b78309ae3cb61f2f46a9b2485814
Hoang Minh <hoangminh30042003@gmail.com>
2025-01-02 08:10:33 +0700

    02-01-2025

---
+++ LeetCode/Coding_challenge_2025_1/02-01-2025/02-01-2025/Program.cs
@@ -0,0 +1,54 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase(["aba", "bcb", "ece", "aa", "e"], [[0, 2], [1, 4], [1, 1]], [2, 3, 0], "Test Case 1");
+        TestCase(["a", "e", "i"], [[0, 2], [0, 1], [2, 2]], [3, 2, 1], "Test Case 2");
+    }
+
+    public static void TestCase(string[] words, int[][] queries, int[] expectation, string description)
+    {
+        var result = VowelStrings(words, queries);
+        var resultString = string.Join(", ", result);
+        var expectationString = string.Join(", ", expectation);
+        Console.WriteLine($"{description}: {(resultString == expectationString ? "Passed" : "Failed")}");
+    }
+
+    public static int[] VowelStrings(string[] words, int[][] queries)
+    {
+        int[] prefixSum = new int[words.Length + 1];
+        int[] result = new int[queries.Length];
+        prefixSum[0] = 0;
+
+        char[] vowel = ['a', 'e', 'i', 'o', 'u'];
+
+        for(int i = 0; i < words.Length; i++)
+        {
+            if (vowel.Contains(words[i][0]) && vowel.Contains(words[i][words[i].Length - 1]))
+            {
+                prefixSum[i + 1] = prefixSum[i] + 1;
+            }
+            else
+            {
+                prefixSum[i + 1] = prefixSum[i];
+            }
+        }
+
+        for(int i = 0; i < queries.Length; i++)
+        {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            if(start == 0)
+            {
+                result[i] = prefixSum[end + 1];
+            }
+            else
+            {
+                result[i] = prefixSum[end + 1] - prefixSum[start];
+            }
+            
+        }
+        
+        return result;
+    }
+}
---
+++ LeetCode/Coding_challenge_2025_1/02-01-2025/README.md
@@ -0,0 +1,13 @@
+# **Count Vowel Strings in Ranges**
+
+You are given a **0-indexed** array of strings `words` and a 2D array of integers `queries`.
+
+Each query **queries[i] = [l<sub>i</sub>, r<sub>i</sub>]** asks us to find the number of strings present in the range l<sub>i</sub> to r<sub>i</sub> (both **inclusive**) of `words` that start and end with a vowel.
+
+_Return an array `ans` of size `queries.length`, where `ans[i]` is the answer to the i<sup>th</sup> query._
+
+**Note** that the vowel letters are `'a'`, `'e'`, `'i'`, `'o'`, and `'u'`.
+
+View full [question](https://leetcode.com/problems/count-vowel-strings-in-ranges?envType=daily-question&envId=2025-01-02)
+
+View my [submission](https://leetcode.com/problems/count-vowel-strings-in-ranges/submissions/1494576453)
