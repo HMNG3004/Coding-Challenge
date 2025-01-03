1b7b2d942871c27b1482cfc0905e9a636dd8937e
Hoang Minh <hoangminh30042003@gmail.com>
2025-01-03 08:27:34 +0700

    03-01-2025

---
+++ LeetCode/Coding_challenge_2025_1/03-01-2025/03-01-2025/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([10, 4, -8, 7], 2, "Test case 1");
+        TestCase([2, 3, 1, 0], 2, "Test case 2");
+    }
+
+    public static void TestCase(int[] nums, int expectation, string description)
+    {
+        var result = WaysToSplitArray(nums);
+        Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int WaysToSplitArray(int[] nums)
+    {
+        long totalSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            totalSum += nums[i];
+        }
+
+        long prefixSum = 0;
+        int totalWays = 0;
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            prefixSum += nums[i];
+            long suffixSum = totalSum - prefixSum;
+
+            if (prefixSum >= suffixSum)
+            {
+                totalWays++;
+            }
+        }
+
+        return totalWays;
+    }
+}
