6d546da6794d80189918871b0e9e470341cc3928
Hoang Minh <hoangminh30042003@gmail.com>
2025-01-04 23:15:26 +0700

    04-01-2025

---
+++ LeetCode/Coding_challenge_2025_1/04-01-2025/04-01-2025/Program.cs
@@ -0,0 +1,61 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase("aabca", 3, "Test Case 1");
+        TestCase("adc", 0, "Test Case 2");
+        TestCase("bbcbaba", 4, "Test Case 3");
+    }
+
+    public static void TestCase(string s, int expectation, string description)
+    {
+        var result = CountPalindromicSubsequence(s);
+        Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int CountPalindromicSubsequence(string s)
+    {
+        var c2pos = new Dictionary<char, List<int>>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!c2pos.ContainsKey(s[i]))
+                c2pos[s[i]] = new List<int>();
+            c2pos[s[i]].Add(i);
+        }
+
+        int res = 0;
+
+        foreach (var centerEntry in c2pos)
+        {
+            char center = centerEntry.Key;
+            var centerPositions = centerEntry.Value;
+
+            foreach (var candidateEntry in c2pos)
+            {
+                char candidate = candidateEntry.Key;
+                var candidatePositions = candidateEntry.Value;
+
+                if (candidate == center)
+                {
+                    if (centerPositions.Count >= 3)
+                        res++;
+                    continue;
+                }
+
+                int leftPos = candidatePositions.First();
+                int rightPos = candidatePositions.Last();
+
+                int index = centerPositions.BinarySearch(leftPos);
+
+                if (index < 0) index = ~index;
+                if (index < centerPositions.Count && leftPos < centerPositions[index] && centerPositions[index] < rightPos)
+                {
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
---
+++ LeetCode/Coding_challenge_2025_1/04-01-2025/README.md
@@ -0,0 +1,15 @@
+# **Unique Length-3 Palindromic Subsequences**
+
+Given a string `s`, return _the number of unique palindromes of length three that are a subsequence of_ `s`.
+
+Note that even if there are multiple ways to obtain the same subsequence, it is still only counted **once**.
+
+A **palindrome** is a string that reads the same forwards and backwards.
+
+A **subsequence** of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+- For example, `"ace"` is a subsequence of `"abcde"`.
+
+View full [question](https://leetcode.com/problems/unique-length-3-palindromic-subsequences?envType=daily-question&envId=2025-01-04)
+
+View my [submission](https://leetcode.com/problems/unique-length-3-palindromic-subsequences/submissions/1497569796)
