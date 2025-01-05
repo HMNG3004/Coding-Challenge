0d1f67ea54739d4e46ccf9869b43e818ae55e426
Hoang Minh <hoangminh30042003@gmail.com>
2025-01-05 17:00:07 +0700

    05-01-2025

---
+++ LeetCode/Coding_challenge_2025_1/05-01-2024/05-01-2024/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TeseCase("abc", [[0, 1, 0], [1, 2, 1], [0, 2, 1]], "ace", "Test Case 1");
+    }
+
+    public static void TeseCase(string s, int[][] shifts, string expectation, string description)
+    {
+        var result = ShiftingLetters(s, shifts);
+        Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static string ShiftingLetters(string s, int[][] shifts)
+    {
+        int n = s.Length;
+        int[] shiftArray = new int[n + 1];
+
+        foreach (var shift in shifts)
+        {
+            int start = shift[0];
+            int end = shift[1];
+            int direction = shift[2];
+
+            int shiftValue = direction == 1 ? 1 : -1;
+
+            shiftArray[start] += shiftValue;
+            shiftArray[end + 1] -= shiftValue;
+        }
+
+        int cumulativeShift = 0;
+        char[] result = s.ToCharArray();
+        for (int i = 0; i < n; i++)
+        {
+            cumulativeShift += shiftArray[i];
+            int effectiveShift = (cumulativeShift % 26 + 26) % 26;
+            result[i] = (char)((result[i] - 'a' + effectiveShift) % 26 + 'a');
+        }
+
+        return new string(result);
+    }
+}
---
+++ LeetCode/Coding_challenge_2025_1/05-01-2024/README.md
@@ -0,0 +1,11 @@
+# **Shifting Letters II**
+
+You are given a string s of lowercase English letters and a 2D integer array shifts where shifts[i] = [starti, endi, directioni]. For every i, shift the characters in s from the index starti to the index endi (inclusive) forward if directioni = 1, or shift the characters backward if directioni = 0.
+
+Shifting a character forward means replacing it with the next letter in the alphabet (wrapping around so that 'z' becomes 'a'). Similarly, shifting a character backward means replacing it with the previous letter in the alphabet (wrapping around so that 'a' becomes 'z').
+
+Return the final string after all such shifts to s are applied.
+
+View full [question](https://leetcode.com/problems/shifting-letters-ii?envType=daily-question&envId=2025-01-05)
+
+View my [submission](https://leetcode.com/problems/shifting-letters-ii/submissions/1498341632)
