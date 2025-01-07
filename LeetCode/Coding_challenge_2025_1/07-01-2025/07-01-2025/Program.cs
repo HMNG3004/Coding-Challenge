bd099ac6d16d5d95a9ba7286ef824ff95d2edbdb
Hoang Minh <hoangminh30042003@gmail.com>
2025-01-07 10:27:02 +0700

    07-01-2025

---
+++ LeetCode/Coding_challenge_2025_1/07-01-2025/07-01-2025/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase(["mass", "as", "hero", "superhero"], ["as", "hero"], "Test Case 1");
+        TestCase(["leetcode", "et", "code"], ["et", "code"], "Test Case 2");
+        TestCase(["blue", "green", "bu"], [], "Test Case 3");
+    }
+
+    public static void TestCase(string[] words, IList<string> expectation, string description)
+    {
+        StringMatching(words);
+        StringMatching2(words);
+    }
+
+    //Brute force solution
+    public static IList<string> StringMatching(string[] words)
+    {
+        var result = new List<string>();
+        foreach(var word in words)
+        {
+            foreach (var other in words)
+            {
+                if (word != other && other.Contains(word))
+                {
+                    result.Add(word);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    //LINQ solution
+    public static IList<string> StringMatching2(string[] words)
+    {
+        return words.Where(x => words.Where(w => w != x).Any(w => w.Contains(x))).ToList();
+    }
+}
---
+++ LeetCode/Coding_challenge_2025_1/07-01-2025/README.md
@@ -0,0 +1,9 @@
+# **String Matching in an Array**
+
+Given an array of string `words`, _return all strings in `words` that is a **substring** of another word._ You can return the answer in **any order**.
+
+A **substring** is a contiguous sequence of characters within a string
+
+View full [question](https://leetcode.com/problems/string-matching-in-an-array?envType=daily-question&envId=2025-01-07)
+
+View my [submission](https://leetcode.com/problems/string-matching-in-an-array/submissions/1500197052)
