d92c1b34ea9acc914352c622e97261485d2635f7
Hoang Minh <hoangminh30042003@gmail.com>
2025-01-09 08:24:28 +0700

    09-01-2025

---
+++ LeetCode/Coding_challenge_2025_1/09-01-2025/09-01-2025/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase(["pay", "attention", "practice", "attend"], "at", 2, "Test Case 1");
+        TestCase(["leetcode", "win", "loops", "success"], "code", 0, "Test Case 2");
+    }
+
+    public static void TestCase(string[] words, string pref, int expectation, string description)
+    {
+        var result = PrefixCount(words, pref);
+        Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int PrefixCount(string[] words, string pref)
+    {
+        int count = 0;
+        foreach (string i in words)
+        {
+            if (i.Length >= pref.Length && i.Substring(0, pref.Length) == pref)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ LeetCode/Coding_challenge_2025_1/09-01-2025/README.md
@@ -0,0 +1,11 @@
+# **Counting Words With a Given Prefix**
+
+You are given an array of strings `words` and a string `pref`.
+
+_Return the number of strings in `words` that contain `pref` as a **prefix**._
+
+A **prefix** of a string `s` is any leading contiguous substring of `s`.
+
+View full [question](https://leetcode.com/problems/counting-words-with-a-given-prefix?envType=daily-question&envId=2025-01-09)
+
+View my [submission](https://leetcode.com/problems/counting-words-with-a-given-prefix/submissions/1502401343)
