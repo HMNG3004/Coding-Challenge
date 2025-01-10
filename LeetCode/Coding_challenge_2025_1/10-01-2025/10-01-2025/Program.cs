89bf0548c6ef963f36ac85aebe0c66467cdd5b07
Hoang Minh <hoangminh30042003@gmail.com>
2025-01-10 14:31:38 +0700

    10-01-2025

---
+++ LeetCode/Coding_challenge_2025_1/10-01-2025/10-01-2025/Program.cs
@@ -0,0 +1,72 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase(["amazon", "apple", "facebook", "google", "leetcode"], ["e", "o"], ["facebook", "google", "leetcode"], "Test case 1");
+        TestCase(["amazon", "apple", "facebook", "google", "leetcode"], ["lo", "eo"], ["google", "leetcode"], "Test case 2");
+    }
+
+    public static void TestCase(string[] words1, string[] words2, IList<String> expectation, string description)
+    {
+        var result = WordSubsets(words1, words2);
+        var resultString = string.Join(", ", result);
+        var expectationString = string.Join(", ", expectation);
+        Console.WriteLine($"{description}: {(resultString == expectationString ? "Passed" : "Failed")}");
+    }
+
+    public static IList<string> WordSubsets(string[] words1, string[] words2)
+    {
+        int[] maxFrequencies = new int[26];
+        HashSet<int> lettersNeeded = new HashSet<int>();
+
+        
+
+        foreach (string word in words2)
+        {
+            int[] wordFrequencies = CountFrequencies(word);
+            for (int i = 0; i < 26; i++)
+            {
+                if (wordFrequencies[i] > maxFrequencies[i])
+                {
+                    maxFrequencies[i] = wordFrequencies[i];
+                    lettersNeeded.Add(i);
+                }
+            }
+        }
+
+        List<int> lettersNeededList = new List<int>(lettersNeeded);
+
+        List<string> universalWords = new List<string>();
+
+        foreach (string word in words1)
+        {
+            int[] wordFrequencies = CountFrequencies(word);
+            bool isUniversal = true;
+            foreach (int i in lettersNeededList)
+            {
+                if (wordFrequencies[i] < maxFrequencies[i])
+                {
+                    isUniversal = false;
+                    break;
+                }
+            }
+            if (isUniversal)
+            {
+                universalWords.Add(word);
+            }
+        }
+
+        return universalWords;
+    }
+
+    public static int[] CountFrequencies(string word)
+    {
+        int[] frequencies = new int[26];
+        foreach (char c in word)
+        {
+            int idx = c - 'a';
+            frequencies[idx]++;
+        }
+        return frequencies;
+    }
+}
---
+++ LeetCode/Coding_challenge_2025_1/10-01-2025/README.md
@@ -0,0 +1,14 @@
+# **Word Subsets**
+
+You are given two string arrays `words1` and `words2`.
+
+A string `b` is a **subset** of string `a` if every letter in `b` occurs in `a` including multiplicity.
+
+For example, `"wrr"` is a subset of `"warrior"` but is not a subset of `"world"`.
+A string `a` from `words1` is **universal** if for every string `b` in `words2`, `b` is a subset of `a`.
+
+Return an array of all the **universal** strings in `words1`. You may return the answer in **any order**.
+
+View full [question](https://leetcode.com/problems/word-subsets?envType=daily-question&envId=2025-01-10)
+
+View my [submission](https://leetcode.com/problems/word-subsets/submissions/1503819200)
