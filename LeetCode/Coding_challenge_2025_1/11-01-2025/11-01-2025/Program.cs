c5876154a4a7d361d62b077d2e3f61861df0a523
Hoang Minh <hoangminh30042003@gmail.com>
2025-01-11 22:40:53 +0700

    11-01-2025

---
+++ LeetCode/Coding_challenge_2025_1/11-01-2025/11-01-2025/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase("annabelle", 2, true, "Test case 1");
+        TestCase("leetcode", 3, false, "Test case 2");
+        TestCase("true", 4, true, "Test case 3");
+    }
+
+    public static void TestCase(string s, int k, bool expectation, string description)
+    {
+        var result = CanConstruct(s, k);
+        Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static bool CanConstruct(string s, int k)
+    {
+        if (s.Length < k) return false;
+
+        char[] chars = s.ToCharArray();
+        Array.Sort(chars);
+        int oddCount = 0;
+
+        for (int i = 0; i < chars.Length;)
+        {
+            char current = chars[i];
+            int count = 0;
+            while (i < chars.Length && chars[i] == current)
+            {
+                count++;
+                i++;
+            }
+            if (count % 2 != 0) oddCount++;
+        }
+
+        return oddCount <= k;
+    }
+}
---
+++ LeetCode/Coding_challenge_2025_1/11-01-2025/README.md
@@ -0,0 +1,7 @@
+# **Construct K Palindrome Strings**
+
+Given a string `s` and an integer `k`, return `true` _if you can use all the characters in `s` to construct `k` palindrome strings or `false` otherwise._
+
+View full [question](https://leetcode.com/problems/construct-k-palindrome-strings?envType=daily-question&envId=2025-01-11)
+
+View my [submission](https://leetcode.com/problems/construct-k-palindrome-strings/submissions/1505190656)
