bf45ca1ddadb80e473c7a40c02509d5b53f6374f
Hoang Minh <hoangminh30042003@gmail.com>
2025-02-01 23:35:00 +0700

    01-02-2025

---
+++ LeetCode/Coding_challenge_2025_2/01-02-2025/01-02-2025/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([1], true, "Test Case 1");
+        TestCase([2, 1, 4], true, "Test Case 2");
+        TestCase([4, 3, 1, 6], false, "Test Case 3");
+    }
+
+    public static void TestCase(int[] nums, bool expectation, string description)
+    {
+        var result = IsArraySpecial(nums);
+        Console.WriteLine($"{description}: {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static bool IsArraySpecial(int[] nums)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if ((nums[i] % 2) == (nums[i + 1] % 2))
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ LeetCode/Coding_challenge_2025_2/01-02-2025/README.md
@@ -0,0 +1,9 @@
+# **Special Array I**
+
+An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
+
+You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
+
+View full [question](https://leetcode.com/problems/special-array-i?envType=daily-question&envId=2025-02-01)
+
+View my [submission](https://leetcode.com/problems/special-array-i/submissions/1527645059)
