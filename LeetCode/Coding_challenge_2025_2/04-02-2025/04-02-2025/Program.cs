ce129925c7d6458537d49d708e7610ebd7fefd34
Hoang Minh <hoangminh30042003@gmail.com>
2025-02-04 17:05:11 +0700

    04-02-2025

---
+++ LeetCode/Coding_challenge_2025_2/04-02-2025/04-02-2025/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        TestCase([10, 20, 30, 5, 10, 50], 65, "Test Case 1");
+        TestCase([10, 20, 30, 40, 50], 150, "Test Case 2");
+        TestCase([12, 17, 15, 13, 10, 11, 12], 33, "Test Case 3");
+    }
+
+    public static void TestCase(int[] nums, int expectation, string description)
+    {
+        var result = MaxAscendingSum(nums);
+        Console.WriteLine($"{description} : {(result == expectation ? "Passed" : "Failed")}");
+    }
+
+    public static int MaxAscendingSum(int[] nums)
+    {
+        if (nums.Length == 0) return 0;
+
+        int maxSum = nums[0], currentSum = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                currentSum += nums[i];
+            }
+            else
+            {
+                maxSum = Math.Max(maxSum, currentSum);
+                currentSum = nums[i];
+            }
+        }
+
+        return Math.Max(maxSum, currentSum);
+    }
+}
---
+++ LeetCode/Coding_challenge_2025_2/04-02-2025/README.md
@@ -0,0 +1,11 @@
+# **Maximum Ascending Subarray Sum**
+
+Given an array of positive integers `nums`, return _the maximum possible sum of an **ascending** subarray in `nums`_.
+
+A subarray is defined as a contiguous sequence of numbers in an array.
+
+A subarray **[nums<sub>l</sub>, nums<sub>l+1</sub>, ..., nums<sub>r-1</sub>, nums<sub>r</sub>]** is ascending if for all `i` where `l <= i < r`, **nums<sub>i</sub> < nums<sub>i+1</sub>.** Note that a subarray of size `1` is **ascending**.
+
+View full [question](https://leetcode.com/problems/maximum-ascending-subarray-sum?envType=daily-question&envId=2025-02-04)
+
+View my [submission](https://leetcode.com/problems/maximum-ascending-subarray-sum/submissions/1530872900)
